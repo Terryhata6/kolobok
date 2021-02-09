c9f8033691ab924579c3817b197f60bb07bf9a7e
Dron Corleone <kefirchik13@gmail.com>
2021-02-09 15:41:40 +0300

    some shit

--- Assets/[Test] Dron/SphereController.cs
+++ Assets/[Test] Dron/SphereController.cs
@@ -22,10 +22,22 @@ public class SphereController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _horAxis = Input.GetAxis(_horizontal);
-        _verAxis = Input.GetAxis(_vertical);
+        //_horAxis = Input.GetAxis(_horizontal);
+        //_verAxis = Input.GetAxis(_vertical);
 
-        _moveVector = new Vector3(Input.GetAxis(_horizontal), 0, Input.GetAxis(_vertical));
+        //_moveVector = new Vector3(Input.GetAxis(_horizontal), 0, Input.GetAxis(_vertical));
+        //_rigidbody.AddForce(_moveVector * _speed * Time.fixedDeltaTime);
+
+        if (Input.GetKey(KeyCode.A)) _horAxis = -1f;
+        else if (_horAxis < 0) _horAxis = 0;
+        if (Input.GetKey(KeyCode.D)) _horAxis = 1f;
+        else if (_horAxis > 0) _horAxis = 0;
+        if (Input.GetKey(KeyCode.S)) _verAxis = -1f;
+        else if (_verAxis < 0) _verAxis = 0;
+        if (Input.GetKey(KeyCode.W)) _verAxis = 1f;
+        else if (_verAxis > 0) _verAxis = 0;
+
+        _moveVector = new Vector3(_horAxis, 0, _verAxis);
         _rigidbody.AddForce(_moveVector * _speed * Time.fixedDeltaTime);
     }
 }
