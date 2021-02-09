46c44a747050d8630a3972209de20ba09caafde3
Dron Corleone <kefirchik13@gmail.com>
2021-02-09 15:11:56 +0300

    Test sphere

---
+++ Assets/[Test] Dron/SphereController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class SphereController : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    [SerializeField] private float _horAxis;
+    [SerializeField] private float _verAxis;
+
+    private Vector3 _moveVector;
+    private Rigidbody _rigidbody;
+
+    private string _horizontal = "Horizontal";
+    private string _vertical = "Vertical";
+
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _horAxis = Input.GetAxis(_horizontal);
+        _verAxis = Input.GetAxis(_vertical);
+
+        _moveVector = new Vector3(Input.GetAxis(_horizontal), 0, Input.GetAxis(_vertical));
+        _rigidbody.AddForce(_moveVector * _speed * Time.fixedDeltaTime);
+    }
+}
