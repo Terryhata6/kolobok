265dc84cef155cbfb9307b8097b66cc57c680d44
SempiternalN7 <60327937+SempiternalN7@users.noreply.github.com>
2021-02-17 22:42:33 +0300

    Snapping Tool
    Илья все знает

---
+++ Assets/Scripts/CustomSnapPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace kolobok
+{
+    public class CustomSnapPoint : MonoBehaviour
+    {
+        public enum ConnectionType
+        {
+            LvlPart
+        }
+
+        public ConnectionType Type;
+
+        private void OnDrawGizmos()
+        {
+            switch (Type)
+            {
+                case ConnectionType.LvlPart:
+                    Gizmos.color = Color.green;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            //Gizmos.color = Color.green;
+            Gizmos.DrawSphere(transform.position, 1.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/SnapSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace kolobok
+{
+    [CreateAssetMenu(fileName = "SnapSettings", menuName = "CreateSO/SnapSettings")]
+    public class SnapSettings : ScriptableObject
+    {
+        [Range(0.0f, 10.0f)]
+        public float SnapDistance;
+    }
+}
